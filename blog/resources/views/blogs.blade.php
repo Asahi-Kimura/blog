191917e8e71c6de0e27555cd4f81e1be1d0cb34f
kimura2nd <asahikimura.ak@gmail.com>
2022-07-25 14:04:20 +0900

    ボタンとタイトルを追加

--- blog/resources/views/blogs.blade.php
+++ blog/resources/views/blogs.blade.php
@@ -12,8 +12,18 @@
   </head>
   <body>
 
-    <header></header>
+    <header>
+      <div class="p-3 mb-2 bg-primary text-white">
+        <div class="d-flex justify-content-between">
+          <div>ブログ</div>
+          <button type="button" class="btn btn-dark">menue</button>
+        </div>
+      </div>
+    
+      
+    </header>
     <main>
+      <h1>ブログ記事一覧</h1>
         <table class="table table-dark table-striped">
             <thead>
                 <tr>
